ed5ecf37b852df00ad5546ec6cf26e8c1be500eb
Manan Kanodia <manan.kanodia@delhivery.com>
2021-05-16 17:15:29 +0530

    new page to display favorites

--- lib/main.dart
+++ lib/main.dart
@@ -124,14 +124,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
-    //     appBar: AppBar(
-    //       title: Text('Welcome to Flutter'),
-    //     ),
-    //     body: Center(
-    //       // child: Text(wordPair.asPascalCase),
-    //       child: RandomWords(),
-    //     ),
-    //   ),
+      //     appBar: AppBar(
+      //       title: Text('Welcome to Flutter'),
+      //     ),
+      //     body: Center(
+      //       // child: Text(wordPair.asPascalCase),
+      //       child: RandomWords(),
+      //     ),
+      //   ),
     );
   }
 }
@@ -150,8 +150,38 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 
